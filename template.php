00d82f5812127d41235599de210eab0b705401d1
palagpatski <albaparticle2005@yahoo.com>
2014-09-03 20:56:25 +0800

    Themed node_submitted.
    Modified the "Submitted by" line in the article pages

--- template.php
+++ template.php
@@ -5,3 +5,11 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * Olender theme.
  */
+
+/*Remove 'Submitted by' on article pages.*/
+
+function olender_preprocess_node(&$variables) {
+  if ($variables['submitted']) {
+    $variables['submitted'] = t('!datetime | !username', array('!username' => $variables['name'], '!datetime' => format_date($variables['node']->created, 'custom', 'F j, Y - g:ia')));
+  }
+}
