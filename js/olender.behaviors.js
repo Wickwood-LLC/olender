01041aacca4f6eb2c8c852e81984888f89e38a39
palagpatski <albaparticle2005@yahoo.com>
2014-09-06 15:56:52 +0800

    Responsive menu
    Added a javascript for responsive menus.
    Font size adjustments on certain breakpoints.

--- js/olender.behaviors.js
+++ js/olender.behaviors.js
@@ -38,23 +38,20 @@
    *   Drupal.settings directly you should use this because of potential
    *   modifications made by the Ajax callback that also produced 'context'.
    */
-  Drupal.behaviors.olenderExampleBehavior = {
+  Drupal.behaviors.olenderResponsiveTabs = {
     attach: function (context, settings) {
-      // By using the 'context' variable we make sure that our code only runs on
-      // the relevant HTML. Furthermore, by using jQuery.once() we make sure that
-      // we don't run the same piece of code for an HTML snippet that we already
-      // processed previously. By using .once('foo') all processed elements will
-      // get tagged with a 'foo-processed' class, causing all future invocations
-      // of this behavior to ignore them.
-      $('.some-selector', context).once('foo', function () {
-        // Now, we are invoking the previously declared theme function using two
-        // settings as arguments.
-        var $anchor = Drupal.theme('olenderExampleButton', settings.myExampleLinkPath, settings.myExampleLinkTitle);
-
-        // The anchor is then appended to the current element.
-        $anchor.appendTo(this);
-      });
+      $('.tabs--primary').before('<div id="menu-button">Menu tabs</div>');
+        $('#menu-button').on('click', function(){
+          $( 'ul.tabs--primary' ).slideToggle( "ease-in-out" );
+        });
     }
   };
-
+  Drupal.behaviors.olenderResponsiveMenu = {
+    attach: function (context, settings) {
+      $('#block-system-main-menu ul.menu').before('<div id="menu-button">Menu</div>');
+        $('#menu-button').on('click', function(){
+          $( 'ul.menu' ).slideToggle( "ease-in-out" );
+        });
+    }
+  };
 })(jQuery);
