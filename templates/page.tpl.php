bc8a65f4a3d355c94238418c723a29c52374628e
palagpatski <albaparticle2005@yahoo.com>
2014-09-09 11:35:21 +0800

    More cleanup and Tweaks
    Page title themed.
    More code cleanup.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -28,7 +28,7 @@
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if ($title): ?>
-        <h2><?php print $title; ?></h2>
+        <h2 class="page-title"><?php print $title; ?></h2>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
       <?php print $messages; ?>
