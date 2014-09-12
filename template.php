51079df199c106b719b7da553d78ff7aff757ed3
palagpatski <albaparticle2005@yahoo.com>
2014-09-12 15:53:54 +0800

    Theme for breadcrumb

--- template.php
+++ template.php
@@ -14,3 +14,16 @@ function olender_preprocess_node(&$variables) {
     $variables['post_author'] = t('!username', array('!username' => $variables['name']));
   }
 }
+
+function theme_breadcrumb($variables) {
+  $breadcrumb = $variables['breadcrumb'];
+
+  if (!empty($breadcrumb)) {
+    // Provide a navigational heading to give context for breadcrumb links to
+    // screen-reader users. Make the heading invisible with .element-invisible.
+    $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+
+    $output .= '<div class="breadcrumb">' . implode(' » ', $breadcrumb) . '</div>';
+    return $output;
+  }
+}
